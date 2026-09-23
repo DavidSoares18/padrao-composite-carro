b1719899c7cb3449925398f0eb42452c088d1a69
David Soares <137106233+Soa1804@users.noreply.github.com>
2026-09-23 18:34:58 -0300

    Atualiza os atributos e metodos da classe Peca

--- carro/Peca.java
+++ carro/Peca.java
@@ -1,4 +1,25 @@
 package carro;
-public class Peca {
-    
+/**
+ * Folha do Composite: parte simples, que tem um peso proprio.
+ * E aqui que o peso e de fato contabilizado (e impresso).
+ */
+public class Peca extends ParteDoCarro {
+    private final int peso;
+
+    public Peca(String nome, int peso) {
+        super(nome);
+        this.peso = peso;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    @Override
+    public int somarPeso(int totalParcial) {
+        int novoTotal = totalParcial + peso;
+        System.out.println("Somando agora o peso de " + getNome() + ": "
+                + peso + ". Total parcial: " + novoTotal);
+        return novoTotal;
+    }
 }
